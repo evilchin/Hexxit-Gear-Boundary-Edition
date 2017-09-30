73ec04a1c2528e75060314f63f6ec2f1ee00a4e4
Shadows_of_Fire <Bward7864@gmail.com>
2017-09-29 22:23:20 -0400

    bump version for release

--- src/main/java/sct/hexxitgear/HexxitGear.java
+++ src/main/java/sct/hexxitgear/HexxitGear.java
@@ -42,7 +42,7 @@ public class HexxitGear {
 
 	public static final String MODID = "hexxitgear";
 	public static final String NAME = "Hexxit Gear";
-	public static final String VERSION = "2.2.0";
+	public static final String VERSION = "2.2.1";
 
 	@Instance
 	public static HexxitGear instance;
