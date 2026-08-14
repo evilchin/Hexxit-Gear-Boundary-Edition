1b726ebb74b16db5bba7aeeb3845051f485db8f4
evilchin <82071239+evilchin@users.noreply.github.com>
2026-08-14 16:36:28 -0500

    Update ModelScaleHelmet.java

--- src/main/java/sct/hexxitgear/model/ModelScaleHelmet.java
+++ src/main/java/sct/hexxitgear/model/ModelScaleHelmet.java
@@ -45,67 +45,66 @@ public class ModelScaleHelmet extends ModelBiped {
 		Shape1.setRotationPoint(0F, 0F, 0F);
 		Shape1.setTextureSize(64, 64);
 		Shape1.mirror = true;
-		setRotation(Shape1, 0F, 0F, 0F);
+		
 		Shape2 = new ModelRenderer(this, 24, 0);
 		Shape2.addBox(-6F, -7F, -2F, 1, 5, 3);
 		Shape2.setRotationPoint(0F, 0F, 0F);
 		Shape2.setTextureSize(64, 64);
 		Shape2.mirror = true;
-		setRotation(Shape2, 0F, 0F, 0F);
+		
 		Shape3 = new ModelRenderer(this, 4, 20);
 		Shape3.addBox(-7F, -8F, 0F, 1, 4, 2);
 		Shape3.setRotationPoint(0F, 0F, 0F);
 		Shape3.setTextureSize(64, 64);
 		Shape3.mirror = true;
-		setRotation(Shape3, 0F, 0F, 0F);
+		
 		Shape4 = new ModelRenderer(this, 2, 2);
 		Shape4.addBox(-9F, -9F, 2F, 2, 1, 1);
 		Shape4.setRotationPoint(0F, 0F, 0F);
 		Shape4.setTextureSize(64, 64);
 		Shape4.mirror = true;
-		setRotation(Shape4, 0F, 0F, 0F);
+		
 		Shape5 = new ModelRenderer(this, 4, 4);
 		Shape5.addBox(-8F, -8F, 1F, 1, 3, 1);
 		Shape5.setRotationPoint(0F, 0F, 0F);
 		Shape5.setTextureSize(64, 64);
 		Shape5.mirror = true;
-		setRotation(Shape5, 0F, 0F, 0F);
+		
 		Shape6 = new ModelRenderer(this, 2, 2);
 		Shape6.addBox(-9F, -7F, 2F, 2, 1, 1);
 		Shape6.setRotationPoint(0F, 0F, 0F);
 		Shape6.setTextureSize(64, 64);
 		Shape6.mirror = true;
-		setRotation(Shape6, 0F, 0F, 0F);
+		
 		Shape7 = new ModelRenderer(this, 24, 0);
 		Shape7.addBox(5F, -7F, -2F, 1, 5, 3);
 		Shape7.setRotationPoint(0F, 0F, 0F);
 		Shape7.setTextureSize(64, 64);
 		Shape7.mirror = true;
-		setRotation(Shape7, 0F, 0F, 0F);
+		
 		Shape8 = new ModelRenderer(this, 4, 20);
 		Shape8.addBox(6F, -8F, 0F, 1, 4, 2);
 		Shape8.setRotationPoint(0F, 0F, 0F);
 		Shape8.setTextureSize(64, 64);
 		Shape8.mirror = true;
-		setRotation(Shape8, 0F, 0F, 0F);
+		
 		Shape9 = new ModelRenderer(this, 4, 4);
 		Shape9.addBox(7F, -9F, 1F, 1, 3, 1);
 		Shape9.setRotationPoint(0F, 1F, 0F);
 		Shape9.setTextureSize(64, 64);
 		Shape9.mirror = true;
-		setRotation(Shape9, 0F, 0F, 0F);
+		
 		Shape10 = new ModelRenderer(this, 2, 2);
 		Shape10.addBox(7F, -9F, 2F, 2, 1, 1);
 		Shape10.setRotationPoint(0F, 0F, 0F);
 		Shape10.setTextureSize(64, 64);
 		Shape10.mirror = true;
-		setRotation(Shape10, 0F, 0F, 0F);
+		
 		Shape11 = new ModelRenderer(this, 2, 2);
 		Shape11.addBox(7F, -7F, 2F, 2, 1, 1);
 		Shape11.setRotationPoint(0F, 0F, 0F);
 		Shape11.setTextureSize(64, 64);
 		Shape11.mirror = true;
-		setRotation(Shape11, 0F, 0F, 0F);
 
 		bipedHead.addChild(Shape1);
         bipedHead.addChild(Shape2);
