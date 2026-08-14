b05829fb49b30f4f0471d05f24a77a014c3b5dfe
evilchin <82071239+evilchin@users.noreply.github.com>
2026-08-14 18:58:29 -0500

    Update ModelHoodHelmet.java

--- src/main/java/sct/hexxitgear/model/ModelHoodHelmet.java
+++ src/main/java/sct/hexxitgear/model/ModelHoodHelmet.java
@@ -20,9 +20,10 @@ package sct.hexxitgear.model;
 
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
 
-public class ModelHoodHelmet extends ModelWtfMojang {
+public class ModelHoodHelmet extends ModelBiped {
 
 	ModelRenderer head;
 	ModelRenderer Shape1;
@@ -30,22 +31,21 @@ public class ModelHoodHelmet extends ModelWtfMojang {
 	ModelRenderer Shape3;
 	ModelRenderer Shape4;
 
-	public ModelHoodHelmet() {
-		textureWidth = 64;
-		textureHeight = 64;
+	public ModelHoodHelmet(float modelSize) {
+		super(modelSize, 0.0F, 64, 64);
 
 		head = new ModelRenderer(this, 0, 0);
 		head.addBox(-4F, -7.5F, -4F, 8, 8, 8);
 		head.setRotationPoint(0F, 0F, 0F);
 		head.setTextureSize(64, 64);
 		head.mirror = true;
-		setRotation(head, 0F, 0F, 0F);
+		
 		Shape1 = new ModelRenderer(this, 48, 0);
 		Shape1.addBox(-3.5F, -8.8F, 5F, 7, 5, 1);
 		Shape1.setRotationPoint(0F, 0F, 0F);
 		Shape1.setTextureSize(64, 64);
 		Shape1.mirror = true;
-		setRotation(Shape1, 0F, 0F, 0F);
+		
 		Shape2 = new ModelRenderer(this, 48, 6);
 		Shape2.addBox(-3F, -8.6F, 6F, 6, 3, 1);
 		Shape2.setRotationPoint(0F, 0F, 0F);
@@ -57,46 +57,17 @@ public class ModelHoodHelmet extends ModelWtfMojang {
 		Shape3.setRotationPoint(0F, 0F, 0F);
 		Shape3.setTextureSize(64, 64);
 		Shape3.mirror = true;
-		setRotation(Shape3, 0F, 0F, 0F);
+		
 		Shape4 = new ModelRenderer(this, 0, 33);
 		Shape4.addBox(-5F, -9F, -5F, 10, 9, 10);
 		Shape4.setRotationPoint(0F, 0F, 0F);
 		Shape4.setTextureSize(64, 64);
 		Shape4.mirror = true;
-		setRotation(Shape4, 0F, 0F, 0F);
+		
 	}
 
 	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-		float scaledUp = f5 + 0.009F;
-		float suLarge = f5 + 0.002F;
-		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		GlStateManager.pushMatrix();
-		if (entity.isSneaking()) GlStateManager.translate(0, .2, 0);
-		head.render(scaledUp);
-		Shape1.render(suLarge);
-		Shape2.render(suLarge);
-		Shape3.render(suLarge);
-		Shape4.render(suLarge);
-		GlStateManager.popMatrix();
-	}
-
-	private void setRotation(ModelRenderer model, float x, float y, float z) {
-		model.rotateAngleX = x;
-		model.rotateAngleY = y;
-		model.rotateAngleZ = z;
-		model.rotationPointY = (this.isSneak) ? 1.0f : 0;
-	}
-
-	@Override
-	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity) {
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
-		float newX = this.bipedHeadwear.rotateAngleX;
-		float newY = this.bipedHeadwear.rotateAngleY;
-		setRotation(head, newX, newY, 0);
-		setRotation(Shape1, newX, newY, 0);
-		setRotation(Shape2, newX, newY, 0);
-		setRotation(Shape3, newX, newY, 0);
-		setRotation(Shape4, newX, newY, 0);
+	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
+		super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale + 0.02F);
 	}
 }
