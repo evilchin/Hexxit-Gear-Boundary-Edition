e3a2592c345ff116d56ab5ab5f83b008f13410f1
evilchin <82071239+evilchin@users.noreply.github.com>
2026-08-14 19:07:51 -0500

    Update ModelHoodHelmet.java

--- src/main/java/sct/hexxitgear/model/ModelHoodHelmet.java
+++ src/main/java/sct/hexxitgear/model/ModelHoodHelmet.java
@@ -68,6 +68,6 @@ public class ModelHoodHelmet extends ModelBiped {
 
 	@Override
 	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale + 0.02F);
+		super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
 	}
 }
