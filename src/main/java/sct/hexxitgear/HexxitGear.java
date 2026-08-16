3239acd3d37d136cdf49a30deb2c9c3178538955
evilchin <82071239+evilchin@users.noreply.github.com>
2026-08-16 09:53:13 -0500

    Update HexxitGear.java

--- src/main/java/sct/hexxitgear/HexxitGear.java
+++ src/main/java/sct/hexxitgear/HexxitGear.java
@@ -41,7 +41,7 @@ public class HexxitGear {
 
 	public static final String MODID = "hexxitgear";
 	public static final String MODNAME = "Hexxit Gear";
-	public static final String VERSION = "3.0.2";
+	public static final String VERSION = "3.0.3";
 
 	@SidedProxy(clientSide = "sct.hexxitgear.proxy.ClientProxy", serverSide = "sct.hexxitgear.proxy.ServerProxy")
 	public static IProxy proxy;
