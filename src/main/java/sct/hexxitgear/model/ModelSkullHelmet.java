fc7be70c9062adf9e36122c653b1d8ea26a9ed4c
evilchin <82071239+evilchin@users.noreply.github.com>
2026-08-16 09:51:15 -0500

    Update ModelSkullHelmet.java

--- src/main/java/sct/hexxitgear/model/ModelSkullHelmet.java
+++ src/main/java/sct/hexxitgear/model/ModelSkullHelmet.java
@@ -12,47 +12,40 @@ import net.minecraft.entity.Entity;
 
 public class ModelSkullHelmet extends ModelBiped {
 
-    ModelRenderer Shape4;
     ModelRenderer Shape1;
     ModelRenderer Shape2;
     ModelRenderer Shape3;
+    ModelRenderer Shape4;
 
     public ModelSkullHelmet(float modelSize) {
         super(modelSize, 0.0F, 64, 32);
 
-        Shape4 = new ModelRenderer(this, 25, 0);
-        Shape4.addBox(4F, -7F, 0F, 3, 2, 2);
-        Shape4.setRotationPoint(0F, 0F, 0F);
-        Shape4.setTextureSize(64, 32);
-        Shape4.mirror = true;
+        bipedHeadwear.isHidden = true;
+
+        bipedHead = new ModelRendererScaled(this, 0, 0, 0.009F);
+        bipedHead.addBox(-4F, -8F, -4F, 8, 8, 8);
+        bipedHead.mirror = true;
         
-        Shape1 = new ModelRenderer(this, 25, 0);
+        Shape1 = new ModelRenderer(this, 25, 0, -0.007F);
         Shape1.addBox(4F, -7F, 0F, 3, 2, 2);
-        Shape1.setRotationPoint(0F, 0F, 0F);
-        Shape1.setTextureSize(64, 32);
         Shape1.mirror = true;
         
-        Shape2 = new ModelRenderer(this, 25, 5);
+        Shape2 = new ModelRenderer(this, 25, 5, -0.007F);
         Shape2.addBox(6F, -8F, 0F, 1, 1, 1);
-        Shape2.setRotationPoint(0F, 0F, 0F);
-        Shape2.setTextureSize(64, 32);
         Shape2.mirror = true;
         
-        Shape3 = new ModelRenderer(this, 25, 5);
+        Shape3 = new ModelRenderer(this, 25, 5, -0.007F);
         Shape3.addBox(-5F, -7F, 0F, 1, 1, 1);
-        Shape3.setRotationPoint(0F, 0F, 0F);
-        Shape3.setTextureSize(64, 32);
         Shape3.mirror = true;
 
-        bipedHead.addChild(Shape4);
+        Shape4 = new ModelRenderer(this, 25, 0, -0.007F);
+        Shape4.addBox(4F, -7F, 0F, 3, 2, 2);
+        Shape4.mirror = true;
+
         bipedHead.addChild(Shape1);
         bipedHead.addChild(Shape2);
         bipedHead.addChild(Shape3);
-    }
-
-    @Override
-    public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale + 0.003F);
+        bipedHead.addChild(Shape4);
     }
 
 }
