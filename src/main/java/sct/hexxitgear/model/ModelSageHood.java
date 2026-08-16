799d5b9631db99e9531ac1be9237db76a8bd4e7d
evilchin <82071239+evilchin@users.noreply.github.com>
2026-08-16 09:09:39 -0500

    Update ModelSageHood.java

--- src/main/java/sct/hexxitgear/model/ModelSageHood.java
+++ src/main/java/sct/hexxitgear/model/ModelSageHood.java
@@ -7,41 +7,34 @@ import net.minecraft.entity.Entity;
 
 public class ModelSageHood extends ModelBiped {
 	
-	ModelRenderer head;
 	ModelRenderer Shape1;
 	ModelRenderer Shape2;
 	ModelRenderer Shape3;
 
 	public ModelSageHood(float modelSize) {
-		super(modelSize, 0.0F, 64, 64);
+        super(modelSize, 0.0F, 64, 64);
 
-		head = new ModelRenderer(this, 0, 0);
-		head.addBox(-4F, -7.5F, -4F, 8, 8, 8);
-		head.setRotationPoint(0F, 0F, 0F);
-		head.setTextureSize(64, 64);
-		head.mirror = true;
+        bipedHeadwear.isHidden = true;
 
-		Shape1 = new ModelRenderer(this, 0, 33);
-		Shape1.addBox(-5F, -9F, -5F, 10, 9, 10);
-		Shape1.setRotationPoint(0F, 0F, 0F);
-		Shape1.setTextureSize(64, 64);
-		Shape1.mirror = true;
+        bipedHead = new ModelRendererScaled(this, 0, 0, 0.009F);
+        bipedHead.addBox(-4F, -7.5F, -4F, 8, 8, 8);
+        bipedHead.mirror = true;
 
-		Shape2 = new ModelRenderer(this, 49, 0);
-		Shape2.addBox(-3F, -9F, 5F, 6, 5, 1);
-		Shape2.setRotationPoint(0F, 0F, 0F);
-		Shape2.setTextureSize(64, 64);
-		Shape2.mirror = true;
+        Shape1 = new ModelRendererScaled(this, 0, 33, -0.007F);
+        Shape1.addBox(-5F, -9F, -5F, 10, 9, 10);
+        Shape1.mirror = true;
 
-		Shape3 = new ModelRenderer(this, 49, 6);
-		Shape3.addBox(-2F, -9F, 6F, 4, 3, 1);
-		Shape3.setRotationPoint(0F, 0F, 0F);
-		Shape3.setTextureSize(64, 64);
-		Shape3.mirror = true;
-	}
+        Shape2 = new ModelRendererScaled(this, 49, 0, -0.007F);
+        Shape2.addBox(-3F, -9F, 5F, 6, 5, 1);
+        Shape2.mirror = true;
 
-	@Override
-	public void render(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		super.render(entityIn, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-	}
+        Shape3 = new ModelRendererScaled(this, 49, 6, -0.007F);
+        Shape3.addBox(-2F, -9F, 6F, 4, 3, 1);
+        Shape3.mirror = true;
+
+        bipedHead.addChild(Shape1);
+        bipedHead.addChild(Shape2);
+        bipedHead.addChild(Shape3);
+    }
+
 }
